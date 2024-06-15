a0735e32df721a85f16d7933669787fa3e6b9f06
HolyMolly <developer.code5417@gmail.com>
2024-06-15 09:33:35 +0300

    Updates

--- main.707d3176c92ec7e492f0.js
+++ main.707d3176c92ec7e492f0.js
@@ -23125,49 +23125,12 @@ exports["default"] = MainLayout;
 
 "use strict";
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 var jsx_runtime_1 = __webpack_require__(2467);
 var react_1 = __importDefault(__webpack_require__(6540));
-var sound_1 = __webpack_require__(2431);
 var images = [
     // Backgrounds
     __webpack_require__(5232),
@@ -23212,46 +23175,35 @@ var Preloader = function () {
             img.src = imageUrl;
         });
     }, [images]);
-    react_1.default.useEffect(function () {
-        if (sound_1.sound) {
-            sound_1.sound.volume = 0; // Громкость звука
-            sound_1.sound.currentTime = 0;
-            sound_1.sound.onerror = function () {
-                console.error("\u041E\u0448\u0438\u0431\u043A\u0430 \u0432\u043E\u0441\u043F\u0440\u043E\u0438\u0437\u0432\u0435\u0434\u0435\u043D\u0438\u044F \u0444\u0430\u0439\u043B\u0430: ".concat(sound_1.sound));
-            };
-            // Попытка воспроизведения звука
-            sound_1.sound.play().catch(function (error) {
-                console.error("\u041E\u0448\u0438\u0431\u043A\u0430 \u043F\u0440\u0438 \u043F\u043E\u043F\u044B\u0442\u043A\u0435 \u0432\u043E\u0441\u043F\u0440\u043E\u0438\u0437\u0432\u0435\u0434\u0435\u043D\u0438\u044F: ".concat(error));
-            });
-        }
-        var loadSound = function () { return __awaiter(void 0, void 0, void 0, function () {
-            var error_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        _a.trys.push([0, 2, , 3]);
-                        return [4 /*yield*/, new Promise(function (resolve, reject) {
-                                sound_1.sound.onloadeddata = resolve;
-                                sound_1.sound.onerror = reject;
-                                sound_1.sound.load();
-                            })];
-                    case 1:
-                        _a.sent();
-                        return [3 /*break*/, 3];
-                    case 2:
-                        error_1 = _a.sent();
-                        console.error('Ошибка при загрузке звуков:', error_1);
-                        return [3 /*break*/, 3];
-                    case 3: return [2 /*return*/];
-                }
-            });
-        }); };
-        loadSound();
-        return function () {
-            sound_1.sound.onloadeddata = null;
-            sound_1.sound.onerror = null;
-        };
-    }, []);
+    // React.useEffect(() => {
+    //   if (sound) {
+    //     sound.volume = 0; // Громкость звука
+    //     sound.currentTime = 0;
+    //     sound.onerror = () => {
+    //       console.error(`Ошибка воспроизведения файла: ${sound}`);
+    //     };
+    //     // Попытка воспроизведения звука
+    //     sound.play().catch((error) => {
+    //       console.error(`Ошибка при попытке воспроизведения: ${error}`);
+    //     });
+    //   }
+    //   const loadSound = async () => {
+    //     try {
+    //       await new Promise((resolve, reject) => {
+    //         sound.onloadeddata = resolve;
+    //         sound.onerror = reject;
+    //         sound.load();
+    //       });
+    //     } catch (error) {
+    //       console.error('Ошибка при загрузке звуков:', error);
+    //     }
+    //   };
+    //   loadSound();
+    //   return () => {
+    //     sound.onloadeddata = null;
+    //     sound.onerror = null;
+    //   };
+    // }, []);
     return (0, jsx_runtime_1.jsx)("div", { className: "" });
 };
 exports["default"] = Preloader;
@@ -23847,7 +23799,6 @@ var Clicker = function () {
                     sound_1.sound.volume = 0.4; // Громкость звука
                     sound_1.sound.currentTime = 0;
                     sound_1.sound.play();
-                    console.log('asd');
                 }
                 setTimeout(function () {
                     setToggleAudio(false);
