63cbd7d8040006a515cb17a2b8d1bdec70ec7e22
HolyMolly <developer.code5417@gmail.com>
2024-06-15 10:44:09 +0300

    Updates

--- main.8a5fb153716cf49a5c40.js
+++ main.8a5fb153716cf49a5c40.js
@@ -23131,7 +23131,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 var jsx_runtime_1 = __webpack_require__(2467);
 var react_1 = __importDefault(__webpack_require__(6540));
-var sound_1 = __webpack_require__(2431);
 var images = [
     // Backgrounds
     __webpack_require__(5232),
@@ -23176,20 +23175,6 @@ var Preloader = function () {
             img.src = imageUrl;
         });
     }, [images]);
-    react_1.default.useEffect(function () {
-        var newSound = new Audio(sound_1.sound);
-        if (newSound) {
-            newSound.volume = 0; // Громкость звука
-            newSound.currentTime = 0;
-            newSound.onerror = function () {
-                console.error("\u041E\u0448\u0438\u0431\u043A\u0430 \u0432\u043E\u0441\u043F\u0440\u043E\u0438\u0437\u0432\u0435\u0434\u0435\u043D\u0438\u044F \u0444\u0430\u0439\u043B\u0430: ".concat(newSound));
-            };
-            // Попытка воспроизведения звука
-            newSound.play().catch(function (error) {
-                console.error("\u041E\u0448\u0438\u0431\u043A\u0430 \u043F\u0440\u0438 \u043F\u043E\u043F\u044B\u0442\u043A\u0435 \u0432\u043E\u0441\u043F\u0440\u043E\u0438\u0437\u0432\u0435\u0434\u0435\u043D\u0438\u044F: ".concat(error));
-            });
-        }
-    }, []);
     return (0, jsx_runtime_1.jsx)("div", { className: "" });
 };
 exports["default"] = Preloader;
@@ -23731,42 +23716,6 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
@@ -23799,9 +23748,7 @@ var Clicker = function () {
         var settings = _a.settings;
         return settings;
     }).clickerTimeout;
-    var audioContextRef = react_2.default.useRef(null);
-    var audioBufferRef = react_2.default.useRef(null);
-    var gainNodeRef = react_2.default.useRef(null);
+    var audioRef = react_2.default.useRef(null);
     var _b = react_2.default.useState([]), particles = _b[0], setParticles = _b[1];
     var _c = react_2.default.useState(false), toggleGif = _c[0], setToggleGif = _c[1];
     var durationGif = 500; // Время действия анимации краба
@@ -23816,12 +23763,9 @@ var Clicker = function () {
             }, durationGif);
         }
         if (is_volume) {
-            if (audioContextRef.current && audioBufferRef.current && gainNodeRef.current) {
-                var source = audioContextRef.current.createBufferSource();
-                source.buffer = audioBufferRef.current;
-                source.connect(gainNodeRef.current);
-                gainNodeRef.current.connect(audioContextRef.current.destination);
-                source.start(0);
+            if (audioRef.current) {
+                audioRef.current.currentTime = 0;
+                audioRef.current.play();
             }
         }
         var _loop_1 = function (idx) {
@@ -23848,55 +23792,7 @@ var Clicker = function () {
     var removeParticle = function (ID) {
         setParticles(function (prev) { return prev.filter(function (particle) { return particle.ID !== ID; }); });
     };
-    react_2.default.useEffect(function () {
-        var initializeAudio = function () { return __awaiter(void 0, void 0, void 0, function () {
-            var AudioContext_1, response, arrayBuffer, _a, error_1;
-            return __generator(this, function (_b) {
-                switch (_b.label) {
-                    case 0:
-                        _b.trys.push([0, 4, , 5]);
-                        AudioContext_1 = window.AudioContext || window.webkitAudioContext;
-                        audioContextRef.current = new AudioContext_1();
-                        return [4 /*yield*/, fetch(sound_1.sound)];
-                    case 1:
-                        response = _b.sent();
-                        if (!response.ok) {
-                            throw new Error('Network response was not ok');
-                        }
-                        return [4 /*yield*/, response.arrayBuffer()];
-                    case 2:
-                        arrayBuffer = _b.sent();
-                        _a = audioBufferRef;
-                        return [4 /*yield*/, audioContextRef.current.decodeAudioData(arrayBuffer)];
-                    case 3:
-                        _a.current = _b.sent();
-                        // Create a GainNode and set the volume
-                        gainNodeRef.current = audioContextRef.current.createGain();
-                        gainNodeRef.current.gain.value = 0.5; // Set volume to 50%
-                        return [3 /*break*/, 5];
-                    case 4:
-                        error_1 = _b.sent();
-                        console.error('Error loading audio file:', error_1);
-                        return [3 /*break*/, 5];
-                    case 5: return [2 /*return*/];
-                }
-            });
-        }); };
-        // Initialize the audio context and buffer on user interaction
-        var handleUserInteraction = function () {
-            if (!audioContextRef.current) {
-                initializeAudio();
-            }
-            else if (audioContextRef.current.state === 'suspended') {
-                audioContextRef.current.resume();
-            }
-        };
-        document.addEventListener('click', handleUserInteraction, { once: true });
-        return function () {
-            document.removeEventListener('click', handleUserInteraction);
-        };
-    }, []);
-    return ((0, jsx_runtime_1.jsxs)("div", { className: (0, classnames_1.default)("\n          w-full\n          flex flex-col justify-center items-center\n          relative\n        ", { 'pointer-events-none': (user === null || user === void 0 ? void 0 : user.spatulaLevel) > (user === null || user === void 0 ? void 0 : user.burgerEnergy) }), children: [(0, jsx_runtime_1.jsxs)("div", { className: (0, classnames_1.default)('crab-tap-clicker', { 'pointer-events-none': clickerTimeout !== 0 }), children: [(0, jsx_runtime_1.jsx)("img", { src: __webpack_require__(6184)("./".concat(toggleGif ? 'crabs-tap' : 'crabs-static', ".gif")), alt: "Crabs" }), (0, jsx_runtime_1.jsx)("div", { className: "crab-tap-area", onTouchStart: clicker }), (0, jsx_runtime_1.jsx)("div", { className: (0, classnames_1.default)('tap-timer-delay', clickerTimeout === 0 ? 'hidden' : 'block'), children: clickerTimeout })] }), (0, jsx_runtime_1.jsx)("ul", { className: "clicker-particles", children: particles === null || particles === void 0 ? void 0 : particles.map(function (particle) { return ((0, react_1.createElement)(ParticleItem_1.default, __assign({}, particle, { key: "particle".concat(particle.ID), removeParticle: removeParticle }))); }) })] }));
+    return ((0, jsx_runtime_1.jsxs)("div", { className: (0, classnames_1.default)("\n          w-full\n          flex flex-col justify-center items-center\n          relative\n        ", { 'pointer-events-none': (user === null || user === void 0 ? void 0 : user.spatulaLevel) > (user === null || user === void 0 ? void 0 : user.burgerEnergy) }), children: [(0, jsx_runtime_1.jsxs)("div", { className: (0, classnames_1.default)('crab-tap-clicker', { 'pointer-events-none': clickerTimeout !== 0 }), children: [(0, jsx_runtime_1.jsx)("img", { src: __webpack_require__(6184)("./".concat(toggleGif ? 'crabs-tap' : 'crabs-static', ".gif")), alt: "Crabs" }), (0, jsx_runtime_1.jsx)("div", { className: "crab-tap-area", onTouchStart: clicker }), (0, jsx_runtime_1.jsx)("div", { className: (0, classnames_1.default)('tap-timer-delay', clickerTimeout === 0 ? 'hidden' : 'block'), children: clickerTimeout }), (0, jsx_runtime_1.jsx)("audio", { ref: audioRef, children: (0, jsx_runtime_1.jsx)("source", { src: sound_1.sound, type: "audio/mpeg" }) })] }), (0, jsx_runtime_1.jsx)("ul", { className: "clicker-particles", children: particles === null || particles === void 0 ? void 0 : particles.map(function (particle) { return ((0, react_1.createElement)(ParticleItem_1.default, __assign({}, particle, { key: "particle".concat(particle.ID), removeParticle: removeParticle }))); }) })] }));
 };
 exports["default"] = Clicker;
 
