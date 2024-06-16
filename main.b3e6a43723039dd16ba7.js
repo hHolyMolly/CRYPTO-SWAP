60e14ab9cd5f9068aba8e9b632f7f0d1525aa0be
HolyMolly <developer.code5417@gmail.com>
2024-06-16 06:57:30 +0300

    Updates

--- main.b3e6a43723039dd16ba7.js
+++ main.b3e6a43723039dd16ba7.js
@@ -40516,6 +40516,7 @@ var allImages = [
     __webpack_require__(5985),
     __webpack_require__(1325),
     __webpack_require__(1056),
+    __webpack_require__(7863),
 ];
 var Preloader = function () {
     var user = (0, _store_1.useAppSelector)(function (_a) {
@@ -41378,22 +41379,82 @@ exports["default"] = Volume;
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 var jsx_runtime_1 = __webpack_require__(2467);
+var react_1 = __importDefault(__webpack_require__(6540));
 __webpack_require__(2843);
+var API_1 = __webpack_require__(9309);
 var _store_1 = __webpack_require__(2482);
+var auth_1 = __webpack_require__(1692);
 var Template_1 = __importDefault(__webpack_require__(123));
 var UI_1 = __webpack_require__(456);
 var Volume_1 = __importDefault(__webpack_require__(7956));
 var Clicker_1 = __importDefault(__webpack_require__(5285));
 function Home() {
+    var dispatch = (0, _store_1.useAppDispatch)();
     var user = (0, _store_1.useAppSelector)(function (_a) {
         var auth = _a.auth;
         return auth;
     }).user;
+    react_1.default.useEffect(function () {
+        function getPosition() {
+            return __awaiter(this, void 0, void 0, function () {
+                var dataPosition;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            if (!(user === null || user === void 0 ? void 0 : user.tgId))
+                                return [2 /*return*/];
+                            return [4 /*yield*/, API_1.API.get('/position/' + (user === null || user === void 0 ? void 0 : user.tgId))];
+                        case 1:
+                            dataPosition = (_a.sent()).data;
+                            dispatch((0, auth_1.setUserPosition)(dataPosition.position));
+                            return [2 /*return*/];
+                    }
+                });
+            });
+        }
+        getPosition();
+    }, [user === null || user === void 0 ? void 0 : user.tgId]);
     return ((0, jsx_runtime_1.jsxs)(Template_1.default, { className: "before:h-[135px] after:h-[270px] bg-[url('@assets/img/bg/home.png')]", children: [(0, jsx_runtime_1.jsxs)("div", { className: "mb-[5px] [@media(min-height:500px)]:mb-[12px] [@media(min-height:730px)]:mb-[16px] [@media(min-height:768px)]:mb-[24px] gap-[8px] [@media(min-height:730px)]:gap-[10px] w-full flex", children: [(0, jsx_runtime_1.jsx)(UI_1.UserBar, { balance: (user === null || user === void 0 ? void 0 : user.balance) || 0, nickName: (user === null || user === void 0 ? void 0 : user.nickName) || 'Username', colorPosition: 1, position: user === null || user === void 0 ? void 0 : user.position, status: "loaded" }), (0, jsx_runtime_1.jsx)(Volume_1.default, {})] }), (0, jsx_runtime_1.jsx)(UI_1.Balance, {}), (0, jsx_runtime_1.jsx)(Clicker_1.default, {})] }));
 }
 exports["default"] = Home;
@@ -42156,6 +42217,14 @@ module.exports = __webpack_require__.p + "img/1ec1982e9d27db887ddb.gif";
 
 "use strict";
 module.exports = __webpack_require__.p + "img/6fe1779ff6fdc01990ed.gif";
+
+/***/ }),
+
+/***/ 7863:
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+"use strict";
+module.exports = __webpack_require__.p + "img/6944e835ee3c6125101c.png";
 
 /***/ }),
 
